fb9bc4c90be9f1b75a0b6f64bbd18272421fda2b
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-23 23:55:46 +0000

    Tue, Jan 23, 2024, 8:55 PM -03:00

--- population/population.c
+++ population/population.c
@@ -4,8 +4,10 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int start_size = get_int("Start size: ");
 
     // TODO: Prompt for end size
+    int end_size = get_int("End size: ");
 
     // TODO: Calculate number of years until we reach threshold
 
