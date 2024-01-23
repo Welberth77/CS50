77987aa4659ce24ee5460385d62669c8dd3c457b
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-23 15:24:36 +0000

    Tue, Jan 23, 2024, 12:24 PM -03:00

--- calculator.c
+++ calculator.c
@@ -6,6 +6,6 @@ int main(void)
     long x = get_long("X: ");
     long y = get_long("Y: ");
 
-    float z =(float) x / (float) y;
-    printf("%lf\n", z);
+    double z =(double) x / (double) y;
+    printf("%.20f\n", z);
 }
