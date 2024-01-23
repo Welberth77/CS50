790b05f8b8182be4081bdf7caeb2d651723a5985
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-23 14:16:22 +0000

    Tue, Jan 23, 2024, 11:16 AM -03:00

--- mario.c
+++ mario.c
@@ -1,8 +1,10 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int n = 5;
+    int n = get_int("Size: ");
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
