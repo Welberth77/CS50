f0fa1f8564734e511a9aad281ba3f368c9f864f3
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-23 23:59:49 +0000

    Tue, Jan 23, 2024, 8:59 PM -03:00

--- population/population.c
+++ population/population.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int calculator_year();
+
 int main(void)
 {
     // TODO: Prompt for start size
@@ -10,7 +12,12 @@ int main(void)
     int end_size = get_int("End size: ");
 
     // TODO: Calculate number of years until we reach threshold
-    int year = 
+    int year = calculator_year();
 
     // TODO: Print number of years
+}
+
+int calculator_year(int start, int end)
+{
+    
 }
