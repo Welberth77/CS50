d1e80053e393ca2667eb0d7083e179ab83f27e1d
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-23 14:49:04 +0000

    Tue, Jan 23, 2024, 11:49 AM -03:00

--- mario.c
+++ mario.c
@@ -1,21 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_grid(int size);
+void print_grid(int size);
+
 int main(void)
+{
+  // Pegar o tamanho
+  int get_size();
+
+  // Mostrando a parede de blocos
+  print_grid(n);
+}
+
+
+int get_size(void)
 {
     int n;
     do
     {
         n = get_int("Size: ");
     }
-    while (n < 1);
+    while(n < 1);
+    return n;
+}
 
-    for (int i = 0; i < n; i++)
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < size; j++)
         {
             printf("#");
         }
-        printf("\n");
+        printf("\n")
     }
 }
