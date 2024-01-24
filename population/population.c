c64b2b0b854cecb5555ae76c4ba626f4bfc52390
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-24 00:17:16 +0000

    Tue, Jan 23, 2024, 9:17 PM -03:00

--- population/population.c
+++ population/population.c
@@ -1,18 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int start_size();
+
 int main(void)
 {
     // TODO: Prompt for start size
-    int start_size;
-    do
-    {
-        start_size = get_int("Start size: ");
-    }
-    while (start_size < 9);
-    return start_size;
+    start_size
 
-    printf("%i\n", start_size);
     // TODO: Prompt for end size
 
     // TODO: Calculate number of years until we reach threshold
@@ -20,3 +15,13 @@ int main(void)
     // TODO: Print number of years
 }
 
+int start_size(void)
+{
+    int start_size;
+    do
+    {
+        start_size = get_int("Start size: ");
+    }
+    while ( start_size < 9);
+    return start_size;
+}
