c4e85cf379c83ec6b18712c796fee0f940092a6c
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-24 15:20:13 +0000

    Wed, Jan 24, 2024, 12:20 PM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int get_height(void);
+
+int main(void)
 {
-    
+    // Pedindo altura, entre 4 e 8 e maior que 0
+    int height = get_height();
+}
+
+int get_height(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while(n < 4 || n > 8);
+    return n;
 }
