9836c1dbf783948d1d2dfdfe0fbdef8c5cb4943e
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-24 15:52:45 +0000

    Wed, Jan 24, 2024, 12:52 PM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -28,7 +28,7 @@ int get_height(void)
 
 void print_pyramid(int h)
 {
-    for (int i = 0; i < h; i++)
+    for (int i = 1; i < h; i++)
     {
         printf("#\n");
         for (int j = 0; j < i; j++)
