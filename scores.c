6131e11f1e16478f94f1d5e21d3b430e8fa8f615
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-29 19:29:15 +0000

    Mon, Jan 29, 2024, 4:29 PM -03:00

--- scores.c
+++ scores.c
@@ -2,9 +2,10 @@
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+    int scores[0] = 72;
+    int scores[1] = 73;
+    int scores[2] = 33;
 
     printf("Média: %f\n", (score1 + score2 + score3) / 3.0);
 }
