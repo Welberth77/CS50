2518ad1ddbda179edfb3201dc4a8083429929723
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-01-30 16:04:00 +0000

    Tue, Jan 30, 2024, 1:04 PM -03:00

--- scores.c
+++ scores.c
@@ -1,25 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Declarando um váriavel constante
 const int N = 3;
+
 float media(int array[]);
 
 int main(void)
 {
-    int scores[3];
+    int scores[N];
     for (int i = 0; i < 3; i++)
     {
         scores[i] = get_int("Score: ");
     }
 
-    printf("Média: %f\n", (scores[0] + scores[1] + scores[2]) / (float) 3);
+    printf("Média: %f\n", media(scores));
 }
 
+// Calculando a média
 float media(int array[])
 {
     int sum = 0;
     for (int i = 0; i < N; i++)
     {
-        
+        sum = sum + array[i];
     }
+    return sum / (float) N;
 }
