c987b1309eb17bb88d509acf0a25b614f073ead9
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-19 18:01:13 +0000

    Fri, Jul 19, 2024, 3:01 PM -03:00

--- buggy.c
+++ buggy.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main (void)
-{
-    for (int i = 0; i < 3; i++)
-    {
+int main (void) {
+    int n =  get_int("Height: ");
+}
+
+void print_column(int height) {
+    for (i = 0; i <= height; i++) {
         printf("#\n");
     }
 }
