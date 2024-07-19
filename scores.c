9a8bea469b8b35a24ab04b722b8cec2580382b5e
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-19 19:07:50 +0000

    Fri, Jul 19, 2024, 4:07 PM -03:00

--- scores.c
+++ scores.c
@@ -4,26 +4,33 @@
 // Declarando um váriavel constante
 const int N = 3;
 
+// Prototipo da função média que usaremos depois
 float media(int array[]);
 
 int main(void)
 {
+    // Criando e definindo tamanho do array
     int scores[N];
     for (int i = 0; i < 3; i++)
     {
+        // Solicitando notas para cada lugar do array
         scores[i] = get_int("Score: ");
     }
 
+    // Printando a média e chamando a função média
     printf("Média: %f\n", media(scores));
 }
 
 // Calculando a média
-float media(int array[]) 
+float media(int array[])
 {
+    // Variável para guardar a soma
     int sum = 0;
     for (int i = 0; i < N; i++)
     {
+        // Somandos cada um dos valores do array
         sum = sum + array[i];
     }
+    // Retornando um valor float
     return sum / (float) N;
 }
