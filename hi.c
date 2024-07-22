628156545fe5094f8f20a841cb98ebd1c08ada81
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-22 21:07:14 +0000

    Mon, Jul 22, 2024, 6:07 PM -03:00

--- hi.c
+++ hi.c
@@ -3,11 +3,14 @@
 
 int main(void)
 {
-    string words[1];
+    // Definindo tamanho do array
+    string words[2];
 
+    // Colocando valores no array
     words[0] = "HI!";
     words[1] = "BYE";
 
+    // Imprimindo valores do array
     printf("%s\n", words[0]);
     printf("%s\n", words[1]);
 }
