f5190dc18174f21c056a1753bc187714716ff0e2
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-22 21:12:02 +0000

    Mon, Jul 22, 2024, 6:12 PM -03:00

--- hi.c
+++ hi.c
@@ -8,9 +8,13 @@ int main(void)
 
     // Colocando valores no array
     words[0] = "HI!";
-    words[1] = "BYE";
+    words[1] = "BYE!";
 
     // Imprimindo valores do array
     printf("%s\n", words[0]);
     printf("%s\n", words[1]);
+
+    // Imprimindo valores do array de outra forma (O que acontece debaixo de capô)
+    printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 }
