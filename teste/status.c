680576923877d05b12aa1cf2deba35a31180e0a7
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-23 21:42:54 +0000

    Tue, Jul 23, 2024, 6:42 PM -03:00

--- teste/status.c
+++ teste/status.c
@@ -6,6 +6,6 @@ int main(int argc, string argv[]) {
         printf("Argumento da linha de comando ausente\n");
         return 1;
     }
-    printf("Hello, %s", argv[1]);
+    printf("Hello, %s\n", argv[1]);
     return 0;
 }
