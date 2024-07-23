c2aaf8691cacf289f268b884abbf0096efcc89ef
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-23 20:26:36 +0000

    Tue, Jul 23, 2024, 5:26 PM -03:00

--- uppercase.c
+++ uppercase.c
@@ -7,9 +7,12 @@ int main(void) {
     string s = get_string("Antes: ");
     printf("Depois: ");
 
+    // Armazenando tamanho da string
     int length = strlen(s);
     for (int i = 0; i < length; i++) {
+        // Verificando se o caractere é minúsculo
         if (s[i] >= 'a' && s[i] <= 'z') {
+            // Transformando em caractere mainúsculo
             printf("%c", s[i] - 32);
         }
         else {
