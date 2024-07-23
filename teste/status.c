81c94ed575a2eef416526aa2b0cd13231a784c18
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-23 21:41:58 +0000

    Tue, Jul 23, 2024, 6:41 PM -03:00

--- teste/status.c
+++ teste/status.c
@@ -1,12 +1,11 @@
-#include <cs50.h
-#include ,stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 
 int main(int argc, string argv[]) {
     if (argc != 2) {
         printf("Argumento da linha de comando ausente\n");
         return 1;
     }
-    else {
-        prinf("Hello, %s", argv[1]);
-    }
+    printf("Hello, %s", argv[1]);
+    return 0;
 }
