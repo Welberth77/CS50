3191646bf862293a1dbd93f462346795769c47e1
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-23 19:02:44 +0000

    Tue, Jul 23, 2024, 4:02 PM -03:00

--- length.c
+++ length.c
@@ -1,13 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
 int main(void) {
+    // Solicitando nome ao usuário
     string name = get_string("Nome: ");
 
+    // Definindo variável inicial
     int n = 0;
+    // Enquanto o caractere não chegar ao \0 continua a contagem
+    // Lembrando que toda string termina com \0
     while (name[n] != '\0') {
+        // Atribuindo valor ao n
         n++;
     }
 
+    // Printando o número de caracteres
     printf("%i\n", n);
 }
