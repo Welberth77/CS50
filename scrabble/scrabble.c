8f7bf77de8020514d5bfeb6dce3a9584a50779dd
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-24 19:53:06 +0000

    Wed, Jul 24, 2024, 4:53 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,13 +2,20 @@
 #import <cs50.h>
 #import <stdio.h>
 
+int compute_score(string word);
+
 int main(void) {
     // Solicitando palavras
     string word_player1 = get_string("Player 1: ");
     string word_player2 = get_string("Player 2: ");
 
     // Calcular resultado
-
+    int score_player1 = compute_score(word_player1);
+    int score_player2 = compute_score(word_player2);
 
     // Imprimir resultado
+}
+
+int compute_score(string word) {
+    return 0;
 }
