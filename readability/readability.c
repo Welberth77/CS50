8e3af7d04fd00845172fe7ccdd4dee59cbb7eb85
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-24 22:17:46 +0000

    Wed, Jul 24, 2024, 7:17 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -31,7 +31,7 @@ int main(void)
     long L = ((long) total_letras / (long) total_palavras) * 100;
     long S = ((long) total_frases / (long) total_palavras) * 100;
 
-    int grade = (int) round(0.0588 * L * 0.296 * S - 15.8);
+    int grade = (int) round(0.0588 * L - 0.296 * S - 15.8);
 
     // Imprimindo nota e nível do texto
     if (grade < 1)
