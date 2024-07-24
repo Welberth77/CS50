470d3e3299cb3e25d6c33129524532cb3bb10ce2
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-24 19:57:25 +0000

    Wed, Jul 24, 2024, 4:57 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,6 +1,8 @@
 // Palavras Cruzadas
 #import <cs50.h>
 #import <stdio.h>
+#import <string.h>
+#import <ctype.h>
 
 // Pontuação das letras do alfabeto
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -21,5 +23,10 @@ int main(void) {
 
 int compute_score(string word) {
     int score = 0;
-    return 0;
+
+    int length = strlen(word);
+    for (int i = 0; i < length; i++) {
+        
+
+    }
 }
