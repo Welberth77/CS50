d1a585bc6b0c1652bc2659c67cc4356a77af2c98
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-24 20:24:33 +0000

    Wed, Jul 24, 2024, 5:24 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,14 +1,18 @@
 // Nivel de leitura do texto
 #import <cs50.h>
 #import <ctype.h>
+#import <math.h>
 #import <stdio.h>
 #import <string.h>
+
 
 int main(void)
 {
     // Entrada do texto
 
     // Calculando nível do texto
+    // Fórmula index = 0.0588 * L - 0.296 * S - 15.8
+    // onde (L) é o número médio de letras por 100 palavras no texto, e (S) é o número médio de frases por 100 palavras no texto.
 
     // Imprimindo nota e nível do texto
 }
