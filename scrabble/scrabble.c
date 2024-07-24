b33ee196a520fed605fff36e99f3d8f16a8590d2
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-24 19:46:10 +0000

    Wed, Jul 24, 2024, 4:46 PM -03:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,9 @@
+// Palavras Cruzadas
+#import <cs50.h>
+#import <stdio.h>
+
+int main(void) {
+    // Solicitando palavras 
+    string word_player1 = get_string("Player 1: ");
+    string word_player2 = get_string("Player 2: ");
+}
