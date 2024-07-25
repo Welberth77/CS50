34ba5ab9e2d069cb5d4bd2e62df596bd686f83b1
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-25 14:09:13 +0000

    Thu, Jul 25, 2024, 11:09 AM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -12,6 +12,7 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
         printf("Usage: ./caesar Key\n");
+        return 1;
     }
     else
     {
@@ -19,10 +20,12 @@ int main(int argc, string argv[])
         if (resultado != true)
         {
             printf("Usage: ./caesar Key\n");
+            return 1;
         }
         else
         {
             printf("%s\n", argv[1]);
+            return 0;
         }
     }
 
