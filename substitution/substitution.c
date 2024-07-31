b59cc975fd6649c73d344d6b8b04f608469d0450
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-07-31 12:22:15 +0000

    Wed, Jul 31, 2024, 9:22 AM -03:00

---
+++ substitution/substitution.c
@@ -0,0 +1,10 @@
+// criptografar mensagens usando uma cifra de substituição. Ou seja, o usuário passando uma chave de 26 caracteres
+#import <cs50.h>
+#import <ctype.h>
+#import <stdio.h>
+#import <string.h>
+
+int main(void)
+{
+    printf("Olá\n");
+}
