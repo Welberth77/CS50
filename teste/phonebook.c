8901d1f0b762846988f121cdd85c707443d01f0d
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-08-01 21:14:52 +0000

    Thu, Aug 1, 2024, 6:14 PM -03:00

--- teste/phonebook.c
+++ teste/phonebook.c
@@ -14,26 +14,26 @@ person;
 int main(void)
 {
     // Criando uma matriz (array) de 3 pessoas
-    person peopole[3];
+    person people[3];
 
     // Atribuindo valores a estrutura
-    person[0].name = "Carter";
-    person[0].numer = "1234";
+    people[0].name = "Carter";
+    people[0].number = "1234";
 
-    person[1].name = "David";
-    person[1].number = "456";
+    people[1].name = "david";
+    people[1].number = "456";
 
-    person[2].name = "John";
-    person[2].number = "7898";
+    people[2].name = "John";
+    people[2].number = "7898";
 
     // Pedindo valor ao usuário
     string name = get_string("Name: ");
 
     for (int i = 0; i < 3; i++)
     {
-        if (strcmp(person[i].name, name) == 0)
+        if (strcmp(people[i].name, name) == 0)
         {
-            printf("Found\n");
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
