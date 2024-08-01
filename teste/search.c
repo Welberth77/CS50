85db022fa6c61389506bef5e07b2592045472983
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-08-01 20:43:27 +0000

    Thu, Aug 1, 2024, 5:43 PM -03:00

--- teste/search.c
+++ teste/search.c
@@ -1,16 +1,38 @@
 // Métodos de pesquisa
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    // Pesquisa com números inteiros
+    // int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
-    int n = get_int("Number: ");
+    // int n = get_int("Number: ");
 
-    for (int i = 0; i < 7; i++)
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     if (numbers[i] == n)
+    //     {
+    //         printf("Found (Encontrado)\n");
+    //         // Vai encerrar o código nestta linha e retornar 0
+    //         return 0;
+    //     }
+    // }
+    // printf("Not found (Não encontrado)\n");
+    // // Retorna 1 e encerra o código
+    // return 1;
+
+
+    // Pesquisa com string
+    // strcmp -> Para comparar duas strings
+    string strings[] = {"banana", "maçã", "pera", "batata", "limão", "manga"};
+
+    string s = get_string("String: ");
+
+    for (int i = 0; i < 6; i++)
     {
-        if (numbers[i] == n)
+        if (strcmp(strings[i], s))
         {
             printf("Found (Encontrado)\n");
             // Vai encerrar o código nestta linha e retornar 0
