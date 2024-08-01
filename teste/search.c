8b70c7152d05e6483dd8f44dc39fbff06d176a9b
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-08-01 20:25:23 +0000

    Thu, Aug 1, 2024, 5:25 PM -03:00

--- teste/search.c
+++ teste/search.c
@@ -7,4 +7,16 @@ int main(void)
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
     int n = get_int("Number: ");
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("Found (Encontrado)\n");
+        }
+        else
+        {
+            printf("Not found (Não encontrado)\n");
+        }
+    }
 }
