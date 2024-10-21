b187488f93926fc13a27aec1ea963006adf33b04
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-10-21 13:47:51 +0000

    Mon, Oct 21, 2024, 10:47 AM -03:00

--- teste/interation.c
+++ teste/interation.c
@@ -1,6 +1,8 @@
 #include <cs50.H>
 #include <stdio.h>
 
+void draw(int n);
+
 int main(void)
 {
     // Pedindo altura
@@ -10,5 +12,12 @@ int main(void)
 
 void draw(int n)
 {
-
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
