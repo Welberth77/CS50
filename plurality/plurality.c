6bdb38ef86b98f02e13d6bcf02ecd64a79fcbc53
Welberth <121064422+Welberth77@users.noreply.github.com>
2024-12-02 22:54:33 +0000

    Mon, Dec 2, 2024, 7:54 PM -03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; i < strlen(candidates); i++){
+    for (int i = 0; i < strlen(candidate); i++){
         if (name == candidates[i]){
             // Adicionar voto ao candidato
             candidates[i].votes += 1;
