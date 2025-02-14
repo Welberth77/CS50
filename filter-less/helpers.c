0d84d51289f5e1b98eb6235f7e4c13da5d4b05b7
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-02-14 23:46:44 +0000

    Fri, Feb 14, 2025, 8:46 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -71,7 +71,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Os valores das linhas devem ser totalmente invertidos
-            RGBTRIPLE temp[0][0] = image[i][j];
+            array temp[0][0] = image[i][j];
             temp[i][j] = image[i][j - (j + 1)];
             image[i][j] = temp[i][j];
         }
