d4e9ecbaed29eea0a67859c83e92a426c4983569
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-06-17 23:55:36 +0000

    Tue, Jun 17, 2025, 8:55 PM -03:00

--- python/sentimental-credit/credit.py
+++ python/sentimental-credit/credit.py
@@ -5,3 +5,5 @@ numeroCartao = get_int("Number: ")
 
 # Algoritmo de Luhn
 # 1 - Multiplique cada outro dígito por 2, começando pelo penúltimo dígito do número, e depois some os dígitos desses produtos.
+# 2 - Adicione a soma à soma dos dígitos que não foram multiplicados por 2.
+# 3 - Se o último dígito do total for 0 (ou, em termos mais formais, se o total módulo 10 for congruente a 0), o número é válido!
