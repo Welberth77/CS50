aafb6f7bb55da9812caa7dc6011e4464120d84cc
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-06-17 23:54:44 +0000

    Tue, Jun 17, 2025, 8:54 PM -03:00

--- python/sentimental-credit/credit.py
+++ python/sentimental-credit/credit.py
@@ -1,4 +1,7 @@
 # Verificar se o cartão é válido
 from cs50 import get_int
 
+numeroCartao = get_int("Number: ")
 
+# Algoritmo de Luhn
+# 1 - Multiplique cada outro dígito por 2, começando pelo penúltimo dígito do número, e depois some os dígitos desses produtos.
