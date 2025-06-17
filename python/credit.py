9019e210cfa8f4a9b87a06ed8ed07fdecc56d30c
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-06-17 23:48:10 +0000

    Tue, Jun 17, 2025, 8:48 PM -03:00

---
+++ python/credit.py
@@ -0,0 +1,9 @@
+from cs50 import get_int
+
+numeroCartao =  get_int("Number: ")
+
+numeroCartaoTemp = list(str(numeroCartao))
+int(numeroCartaoTemp)
+
+for i in range(len(numeroCartaoTemp -1 , -1)):
+    numeroCartaoTemp[i] * 2
