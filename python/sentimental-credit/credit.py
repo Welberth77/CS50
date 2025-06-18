264fb912d93940ff74336807a056b75027ccbf31
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-06-18 00:01:37 +0000

    Tue, Jun 17, 2025, 9:01 PM -03:00

--- python/sentimental-credit/credit.py
+++ python/sentimental-credit/credit.py
@@ -16,3 +16,6 @@ numeroCartao = get_int("Number: ")
 # 1 - Multiplique cada outro dígito por 2, começando pelo penúltimo dígito do número, e depois some os dígitos desses produtos.
 # 2 - Adicione a soma à soma dos dígitos que não foram multiplicados por 2.
 # 3 - Se o último dígito do total for 0 (ou, em termos mais formais, se o total módulo 10 for congruente a 0), o número é válido!
+
+for i in range(numeroCartao):
+    print(numeroCartao[i])
