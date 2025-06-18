276bb517135b32d7d162248382df2005a2f41123
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-06-18 00:00:02 +0000

    Tue, Jun 17, 2025, 9:00 PM -03:00

--- python/sentimental-credit/credit.py
+++ python/sentimental-credit/credit.py
@@ -3,6 +3,15 @@ from cs50 import get_int
 
 numeroCartao = get_int("Number: ")
 
+# America Express
+# 15 digitos, começa com 34 ou 37
+
+# Mastercard
+# 16 Digitos, comeca com 51, 52, 53, 54 ou 55
+
+# Visa
+# 13 ou 16 digitos, comeca com 4
+
 # Algoritmo de Luhn
 # 1 - Multiplique cada outro dígito por 2, começando pelo penúltimo dígito do número, e depois some os dígitos desses produtos.
 # 2 - Adicione a soma à soma dos dígitos que não foram multiplicados por 2.
