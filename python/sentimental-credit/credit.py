c95b830c2b0149f7d856453bba8389c44b2ffa4c
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-06-18 00:21:15 +0000

    Tue, Jun 17, 2025, 9:21 PM -03:00

--- python/sentimental-credit/credit.py
+++ python/sentimental-credit/credit.py
@@ -19,8 +19,12 @@ numeroCartao = get_int("Number: ")
 
 tamanhoNumero = len(str(numeroCartao))
 numeroInvertido = str(numeroCartao)[::-1]
+lista = []
 
 # Começa na penúltima posição e vai até o inicio de 2 em 2
 for i in range(1, len(numeroInvertido), 2):
     print(f"{numeroInvertido[i]}", end=" ")
-print()
+    lista.append(numeroInvertido[i] * 2)
+    print(lista)
+
+print(lista)
