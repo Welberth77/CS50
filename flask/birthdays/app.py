9c1375d4d5fd554af0a0b50973f0e606258924a2
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-07-28 13:13:43 +0000

    Mon, Jul 28, 2025, 10:13 AM -03:00

--- flask/birthdays/app.py
+++ flask/birthdays/app.py
@@ -31,6 +31,9 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
+        if not name or not month or not day:
+            return redirect("/")
+
         db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
