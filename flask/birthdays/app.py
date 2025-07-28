897c5a78ca5f73b4a04295f847b245dabcd5610b
Welberth <121064422+Welberth77@users.noreply.github.com>
2025-07-28 12:56:17 +0000

    Mon, Jul 28, 2025, 9:56 AM -03:00

--- flask/birthdays/app.py
+++ flask/birthdays/app.py
@@ -27,6 +27,9 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        request.form.get("name")
+        request.form.get("month")
+        request.form.get("day")
 
         return redirect("/")
 
